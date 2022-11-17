e3edc0c6b7471961c9558dc3b2e1eb27375147d9
Mikhail Zerafa <mize01@betssongroup.com>
2022-11-17 17:46:56 +0100

    fixed allow synthetic imports

--- json-evolve/build/jsonDeconstructed.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.JsonDeconstructed = void 0;
-class JsonDeconstructed {
-    constructor(entities) {
-        this.entities = entities;
-    }
-}
-exports.JsonDeconstructed = JsonDeconstructed;
--- json-evolve/build/tempModel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tempModel = void 0;
-class tempModel {
-}
-exports.tempModel = tempModel;
